8285862072dda575d37f2fe14d43596c8e020156
Alexandre <oliveiralexandre0@gmail.com>
2020-06-13 22:15:47 -0300

    second commit

---
+++ app/Http/Controllers/Admin/PlanController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Support\Str;
+use App\Http\Controllers\Controller;
+use App\Models\Plan;
+use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class PlanController extends Controller
+{
+    private $repository;
+
+    public function __construct(Plan $plan)
+    {
+        $this->repository = $plan;
+    }
+
+    public function index()
+    {
+        $plans = $this->repository->latest()->paginate();
+
+        return view('admin.pages.plans.index', [
+            'plans' => $plans,
+        ]);
+    }
+
+    public function create()
+    {
+        return view('admin.pages.plans.create');
+    }
+
+    public function store(Request $request)
+    {
+        Alert::success('Plano Cadastrado com Sucesso!');
+        //dd($request->all());
+        //$this->repository->create($request->all());
+        $data = $request->all();
+        $data['url'] = Str::kebab($request->name);
+        $this->repository->create($data);
+
+        return redirect()->route('plans.index');
+    }
+
+    public function show($url)
+    {
+        $plan = $this->repository->where('url', $url)->first();
+
+        if (!$plan)
+            return redirect()->back();
+
+        return view('admin.pages.plans.show', [
+            'plan' => $plan
+        ]);
+    }
+
+    public function destroy($url)
+    {
+        $plan = $this->repository->where('url', $url)->first();
+        Alert::success('Plano Deletado com Sucesso!');
+        if (!$plan)
+            return redirect()->back();
+
+        $plan->delete();
+
+        return redirect()->route('plans.index');
+    }
+
+    public function search(Request $request)
+    {
+        $filters = $request->except('_token');
+
+        $plans = $this->repository->search($request->filter);
+
+        return view('admin.pages.plans.index', [
+            'plans' => $plans,
+            'filters' => $filters,
+        ]);
+    }
+
+    public function edit($url)
+    {
+        $plan = $this->repository->where('url', $url)->first();
+       
+        if (!$plan)
+            return redirect()->back();
+
+        return view('admin.pages.plans.edit', [
+            'plan' => $plan
+        ]);
+    }
+
+    public function update(Request $request, $url)
+    {
+        $plan = $this->repository->where('url', $url)->first();
+       
+        if (!$plan)
+            return redirect()->back();
+
+        $plan->update($request->all());
+
+        Alert::success('Plano Atualizado com Sucesso!');
+
+        return redirect()->route('plans.index');
+    }
+
+}
